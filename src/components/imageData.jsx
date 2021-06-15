b690be3feabe6ef071fda505d7eae58158cbe868
Josh Madakor <josh.madakor@gmail.com>
2021-06-14 20:49:39 -0700

    Re-arranged some cards

--- src/components/imageData.jsx
+++ src/components/imageData.jsx
@@ -84,7 +84,7 @@ class ImageData extends Component {
         //this.resetState();
         const {data} = await axios({
             method: 'get',
-            url: 'http://127.0.0.1:3030/4chanraw' //https://c964-josh.azurewebsites.net/4chanraw
+            url: 'https://c964-josh.azurewebsites.net/4chanraw' //http://127.0.0.1:3030/4chanraw
           })
           console.log(data)
           this.setState({data: data,imageBlur:"30px"});
@@ -120,105 +120,94 @@ class ImageData extends Component {
 
                 </center>
             
+            
             <div className="row" style={{width: "90%", height: "90%", paddingLeft: "15%"}}>
-                <div className="col-6 col-md-4">
-
-            
-
-          {/* Adult Content */}
-            <div className="card" style={{width: "18rem", fontSize: "90%", borderColor: "#f66d7a"}}>
-                <div className="card-body">
-                <h5 className="card-title">Adult Content</h5>
-                <ul style={{marginBottom: 0}}>
-            {(this.state.data.hasOwnProperty('adult') && this.state.data.adult.hasOwnProperty('adultScore')) && (
-                <React.Fragment>
-                    {this.state.data.adult.adultScore > 0.5 ? <li key={this.state.data.adult.adultScore}> <font style={{fontWeight: "bold", fontSize: "110%", textShadow:"0 0 3px #FFFF00"}} className="text-danger">Adult Content - {parseFloat(this.state.data.adult.adultScore * 100).toFixed(2)}%</font></li> : <li key={this.state.data.adult.adultScore}>Adult Content - {parseFloat(this.state.data.adult.adultScore * 100).toFixed(2)}%</li>}
-                    {this.state.data.adult.racyScore > 0.5 ? <li key={this.state.data.adult.racyScore}> <font style={{fontWeight: "bold", fontSize: "110%", textShadow:"0 0 3px #FFFF00"}} className="text-danger">Sus Content - {parseFloat(this.state.data.adult.racyScore * 100).toFixed(2)}%</font></li> : <li key={this.state.data.adult.racyScore}>Sus Content - {parseFloat(this.state.data.adult.racyScore * 100).toFixed(2)}%</li>}
-                    {this.state.data.adult.goreScore > 0.5 ? <li key={this.state.data.adult.goreScore}> <font style={{fontWeight: "bold", fontSize: "110%", textShadow:"0 0 3px #FFFF00"}} className="text-danger">Gore - {parseFloat(this.state.data.adult.goreScore * 100).toFixed(2)}%</font></li> : <li key={this.state.data.adult.goreScore}>Gore - {parseFloat(this.state.data.adult.goreScore * 100).toFixed(2)}%</li>}
-                </React.Fragment>
-            )}</ul>
-                </div>
-            </div>
-
-
-
-            {/* General Info */}
-            <div className="card" style={this.cardStyle}>
-                <div className="card-body">
-                <h5 className="card-title">General Info</h5>
-                <ul style={{marginBottom: 0}}>
-                {this.state.data.hasOwnProperty('categories') && (
-                    this.state.data.categories.map(c => (
-                        <li key={c.name}>
-                            {c.name.replace("_"," ")} - {parseFloat(c.score * 100).toFixed(2)}%
-                        </li>
-                    )
-                ))}</ul>
-                </div>
-            </div>
                 
-            
-            
-
-
-           
-            
-            {/* Other Info */}
-            <div className="card" style={this.cardStyle}>
-                <div className="card-body">
-                <h5 className="card-title">Other Info</h5>
-                <ul style={{marginBottom: 0}}>
-            {(this.state.data.hasOwnProperty('description') && this.state.data.description.hasOwnProperty('captions')) && (
-                this.state.data.description.captions.map( caption => (
-                <li key={caption.text}>{caption.text}</li>
-            )))}
-            </ul>
-                </div>
-            </div>
-
-         
-
-
-            {/* Tags */}
-            <div className="card" style={this.cardStyle}>
-                <div className="card-body">
-                <h5 className="card-title">Tags</h5>
-                <ul style={{marginBottom: 0}}>
-                <li style={{listStyle: "none"}}>
-                    {(this.state.data.hasOwnProperty('description') && this.state.data.description.hasOwnProperty('tags')) && (
-                    this.state.data.description.tags.map( tag => (
-                        <span key={tag}>#{tag} </span>
-                    )))}
-                    {/*this.state.data.description.tags.length === 0 &&<span>None</span>*/}                
-                </li>
-            </ul>
-                </div>
-            </div>
-
-
-            {/* Analytics */}
-            <div className="card" style={{width: "18rem", fontSize: "90%", backgroundColor: "#f0e0d6"}}>
-                <div className="card-body">
-                <h5 className="card-title">Lifetime Analytics</h5>
-                    <ul style={{marginBottom: 0}}>
-                    <li style={{listStyle: "none"}}>Total Images Analyzed: {(this.state.data.hasOwnProperty("analytics") && (this.state.data.analytics.count > 0)) && this.state.data.analytics.count}</li>
-                    <li style={{listStyle: "none"}}>Total Clean: {(this.state.data.hasOwnProperty("analytics") && (this.state.data.analytics.count > 0)) && this.state.data.analytics.count - this.state.data.analytics.nsfw}</li>
-                    <li style={{listStyle: "none"}}>Total NSFW: {((this.state.data.hasOwnProperty("analytics") && (this.state.data.analytics.count > 0)) && this.state.data.analytics.nsfw)}</li>
-                    <li style={{listStyle: "none"}}>NSFW Percentage: {(this.state.data.hasOwnProperty("analytics") && (this.state.data.analytics.count > 0)) && ((this.state.data.analytics.nsfw / this.state.data.analytics.count)*100).toFixed(2)}%</li>
-                    </ul>
-                </div>
-            </div>
-
-            
-
-            </div>
-
             <div className="col-12 col-sm-6 col-md-8">
 
-            <center>
-                <img alt="Random visual grabbed from 4chan.org/b/" src={(this.state.data.hasOwnProperty("mirrorimageurl")) && (this.state.data.mirrorimageurl.length > 0) && (this.state.data.mirrorimageurl )} onClick={this.toggleBlur} style={{maxWidth: "90%", height: "auto", filter: `blur(${this.state.imageBlur})`}} />
+                <center>
+                    <img alt="Random visual grabbed from 4chan.org/b/" src={(this.state.data.hasOwnProperty("mirrorimageurl")) && (this.state.data.mirrorimageurl.length > 0) && (this.state.data.mirrorimageurl )} onClick={this.toggleBlur} style={{maxWidth: "90%", height: "auto", filter: `blur(${this.state.imageBlur})`}} />
                 </center>
             </div>
+                
+                <div className="col-6 col-md-4">
+
+                    {/* Analytics */}
+                    <div className="card" style={{width: "18rem", fontSize: "90%", backgroundColor: "#f0e0d6"}}>
+                        <div className="card-body">
+                        <h5 className="card-title">Lifetime Analytics</h5>
+                            <ul style={{marginBottom: 0}}>
+                            <li style={{listStyle: "none"}}>Total Images Analyzed: {(this.state.data.hasOwnProperty("analytics") && (this.state.data.analytics.count > 0)) && this.state.data.analytics.count}</li>
+                            <li style={{listStyle: "none"}}>Total Clean: {(this.state.data.hasOwnProperty("analytics") && (this.state.data.analytics.count > 0)) && this.state.data.analytics.count - this.state.data.analytics.nsfw}</li>
+                            <li style={{listStyle: "none"}}>Total NSFW: {((this.state.data.hasOwnProperty("analytics") && (this.state.data.analytics.count > 0)) && this.state.data.analytics.nsfw)}</li>
+                            <li style={{listStyle: "none"}}>NSFW Percentage: {(this.state.data.hasOwnProperty("analytics") && (this.state.data.analytics.count > 0)) && ((this.state.data.analytics.nsfw / this.state.data.analytics.count)*100).toFixed(2)}%</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    {/* Adult Content */}
+                    <div className="card" style={{width: "18rem", fontSize: "90%", borderColor: "#f66d7a"}}>
+                        <div className="card-body">
+                        <h5 className="card-title">Adult Content</h5>
+                        <ul style={{marginBottom: 0}}>
+                    {(this.state.data.hasOwnProperty('adult') && this.state.data.adult.hasOwnProperty('adultScore')) && (
+                        <React.Fragment>
+                            {this.state.data.adult.adultScore > 0.5 ? <li key={this.state.data.adult.adultScore}> <font style={{fontWeight: "bold", fontSize: "110%", textShadow:"0 0 3px #FFFF00"}} className="text-danger">Adult Content - {parseFloat(this.state.data.adult.adultScore * 100).toFixed(2)}%</font></li> : <li key={this.state.data.adult.adultScore}>Adult Content - {parseFloat(this.state.data.adult.adultScore * 100).toFixed(2)}%</li>}
+                            {this.state.data.adult.racyScore > 0.5 ? <li key={this.state.data.adult.racyScore}> <font style={{fontWeight: "bold", fontSize: "110%", textShadow:"0 0 3px #FFFF00"}} className="text-danger">Sus Content - {parseFloat(this.state.data.adult.racyScore * 100).toFixed(2)}%</font></li> : <li key={this.state.data.adult.racyScore}>Sus Content - {parseFloat(this.state.data.adult.racyScore * 100).toFixed(2)}%</li>}
+                            {this.state.data.adult.goreScore > 0.5 ? <li key={this.state.data.adult.goreScore}> <font style={{fontWeight: "bold", fontSize: "110%", textShadow:"0 0 3px #FFFF00"}} className="text-danger">Gore - {parseFloat(this.state.data.adult.goreScore * 100).toFixed(2)}%</font></li> : <li key={this.state.data.adult.goreScore}>Gore - {parseFloat(this.state.data.adult.goreScore * 100).toFixed(2)}%</li>}
+                        </React.Fragment>
+                    )}</ul>
+                        </div>
+                    </div>
+
+                    {/* General Info */}
+                    <div className="card" style={this.cardStyle}>
+                        <div className="card-body">
+                        <h5 className="card-title">General Info</h5>
+                        <ul style={{marginBottom: 0}}>
+                        {this.state.data.hasOwnProperty('categories') && (
+                            this.state.data.categories.map(c => (
+                                <li key={c.name}>
+                                    {c.name.replace("_"," ")} - {parseFloat(c.score * 100).toFixed(2)}%
+                                </li>
+                            )
+                        ))}</ul>
+                        </div>
+                    </div>
+                
+                    {/* Other Info */}
+                    <div className="card" style={this.cardStyle}>
+                        <div className="card-body">
+                        <h5 className="card-title">Other Info</h5>
+                        <ul style={{marginBottom: 0}}>
+                    {(this.state.data.hasOwnProperty('description') && this.state.data.description.hasOwnProperty('captions')) && (
+                        this.state.data.description.captions.map( caption => (
+                        <li key={caption.text}>{caption.text}</li>
+                    )))}
+                    </ul>
+                        </div>
+                    </div>
+
+                    {/* Tags */}
+                    <div className="card" style={this.cardStyle}>
+                        <div className="card-body">
+                        <h5 className="card-title">Tags</h5>
+                        <ul style={{marginBottom: 0}}>
+                        <li style={{listStyle: "none"}}>
+                            {(this.state.data.hasOwnProperty('description') && this.state.data.description.hasOwnProperty('tags')) && (
+                            this.state.data.description.tags.map( tag => (
+                                <span key={tag}>#{tag} </span>
+                            )))}
+                            {/*this.state.data.description.tags.length === 0 &&<span>None</span>*/}                
+                        </li>
+                    </ul>
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+                
             <br/><br/>
 
             </div>
